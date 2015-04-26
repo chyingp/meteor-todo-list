9925442677ea1f3201b07bfcb5f01a84ad25a989
casperchen(陈映平) <chyingp@gmail.com>
2015-04-26 23:58:04 +0800

    update

--- meteor-todo-list.js
+++ meteor-todo-list.js
@@ -1,27 +1,3 @@
-// if (Meteor.isClient) {
-//   // counter starts at 0
-//   Session.setDefault('counter', 0);
-
-//   Template.hello.helpers({
-//     counter: function () {
-//       return Session.get('counter');
-//     }
-//   });
-
-//   Template.hello.events({
-//     'click button': function () {
-//       // increment the counter when button is clicked
-//       Session.set('counter', Session.get('counter') + 1);
-//     }
-//   });
-// }
-
-// if (Meteor.isServer) {
-//   Meteor.startup(function () {
-//     // code to run on server at startup
-//   });
-// }
-
 var Tasks = new Mongo.Collection("tasks");
 
 if( Meteor.isClient ){
@@ -37,10 +13,9 @@ if( Meteor.isClient ){
       var $input = template.$('.js-text'),
           value = $input.val();
 
-      Tasks.insert({
-        text: value,
-        createdAt: new Date()
-      });
+      Tasks.insert({text: value, createdAt: new Date()});
+
+      $input.val('');
     }
   });
 
